40b3625c8d2fbbaecef834a50edd04d85f03d26f
Rob Watson <rob.watson@me.com>
2022-10-10 09:53:25 +0100

    Colour delete icon, change to cross instead of trash can

--- src/frontend/src/Components/Icons/DeleteIcon.js
+++ src/frontend/src/Components/Icons/DeleteIcon.js
@@ -1,3 +1,3 @@
-const DeleteIcon = ({className, ...props}) => <span {...props} className={`${className} fas fa-trash fa-fw`}></span>
+const DeleteIcon = ({className, ...props}) => <span {...props} className={`${className} fa fa-times fa-fw`}></span>
 
 export default DeleteIcon
--- src/frontend/src/Components/TodoItem.js
+++ src/frontend/src/Components/TodoItem.js
@@ -6,7 +6,7 @@ const TodoItem = ({todo, markTodoComplete, deleteTodo, hardDeleteTodo}) => (
     <li className={`todo-item ${todo.deleted ? 'deleted' : ''} ${todo.complete ? 'complete' : ''}`}>
         <Checkbox checked={todo.complete} onChange={e => markTodoComplete(todo.id)} />
         <span className="todo-name">{todo.name}</span>
-        { todo.deleted && <DeleteHardIcon title="Permanently delete" onClick={(e => hardDeleteTodo(todo.id))} /> }
+        { todo.deleted && <DeleteHardIcon className="todo-delete-hard" title="Permanently delete" onClick={(e => hardDeleteTodo(todo.id))} /> }
         <DeleteIcon className="todo-delete" onClick={e => deleteTodo(todo.id)} />
     </li>
 )
